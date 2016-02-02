3606a900f3a5c613f2b19baad5b2525cbd6e2897
Daniel Schofield <daniel-schofield93@hotmail.com>
2016-02-02 12:01:26 +1100

    Update uses-permission to allow for internet connection

--- app/src/main/java/com/ait/aitendance/SignupActivity.java
+++ app/src/main/java/com/ait/aitendance/SignupActivity.java
@@ -2,6 +2,7 @@ package com.ait.aitendance;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -13,11 +14,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -191,6 +203,92 @@ public class SignupActivity extends AppCompatActivity {
             this.CurrentActivity = CurrentActivity;
         }
 
+        /*
+        private TextView statusField,roleField;
+        private Context context;
+        private int byGetOrPost = 0;
+
+        //flag 0 means get and 1 means post.(By default it is get.)
+        public SigninActivity(Context context,TextView statusField,TextView roleField,int flag) {
+            this.context = context;
+            this.statusField = statusField;
+            this.roleField = roleField;
+            byGetOrPost = flag;
+        }
+
+
+        @Override
+        protected String doInBackground(String... arg0) {
+            if(byGetOrPost == 0){ //means by Get Method
+
+                try{
+                    String username = (String)arg0[0];
+                    String password = (String)arg0[1];
+                    String link = "http://myphpmysqlweb.hostei.com/login.php?username="+username+"& password="+password;
+
+                    URL url = new URL(link);
+                    HttpClient client = new DefaultHttpClient();
+                    HttpGet request = new HttpGet();
+                    request.setURI(new URI(link));
+                    HttpResponse response = client.execute(request);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while ((line = in.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+                    in.close();
+                    return sb.toString();
+                }
+
+                catch(Exception e){
+                    return new String("Exception: " + e.getMessage());
+                }
+            }
+            else{
+                try{
+                    String username = (String)arg0[0];
+                    String password = (String)arg0[1];
+
+                    String link="http://myphpmysqlweb.hostei.com/loginpost.php";
+                    String data  = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8");
+                    data += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+                    wr.write( data );
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    // Read Server Response
+                    while((line = reader.readLine()) != null)
+                    {
+                        sb.append(line);
+                        break;
+                    }
+                    return sb.toString();
+                }
+                catch(Exception e){
+                    return new String("Exception: " + e.getMessage());
+                }
+            }
+        }
+
+
+
+
+        */
         @Override
         protected Void doInBackground(Void... params) {
             //Note, probably should pass these next 2 values in at constructor
@@ -208,7 +306,7 @@ public class SignupActivity extends AppCompatActivity {
             parameters.add(new BasicNameValuePair("password", password));
             parameters.add(new BasicNameValuePair("uuid",androidId));
             parameters.add(new BasicNameValuePair("studentNo",studentNo));
-            JSONObject json = jsonParser.getJSONFromUrl("http://androidapi.mattconcepts.com/register.php", parameters);
+            JSONObject json = jsonParser.getJSONFromUrl("http://ait.interactivehippo.com.au/advancedstudio2/mobileapp/register.php", parameters);
             if(json != null)
             {
                 try{
